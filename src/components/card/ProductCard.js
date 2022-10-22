cc77d3ab47df8feba0b599803711dba8e55b219c
IrhamFR <irham.f.rufdo@gmail.com>
2022-10-23 01:33:57 +0700

    update

--- src/components/card/ProductCard.js
+++ src/components/card/ProductCard.js
@@ -5,13 +5,13 @@ import convertRupiah from "rupiah-format";
 
 export default function ProductCard({ item }) {
   return (
-    <Link to={`/product/` + item.id} style={{ textDecoration: "none", width: "100px", height: "150px"}}>
+    <Link to={`/product/` + item.id} style={{ textDecoration: "none"}}>
       <div className="card-product mt-3">
         <img src={item.image} className="img-fluid img-rounded" alt={item.name} />
         <div className="p-2">
           <div className="text-header-product-item">{item.name}</div>
           {/* <div className="text-product-item">{convertRupiah.convert(item.buy)}</div> */}
-          <div className="text-product-item">{convertRupiah.convert(item.buy)}</div>
+          <div className="text-product-item">{convertRupiah.convert(item.sell)}</div>
           <div className="text-product-item">Stock : {item.qty}</div>
         </div>
       </div>
