2143902ca27cca4b05ecd485f1eaa4e438204225
IrhamFR <irham.f.rufdo@gmail.com>
2022-10-22 23:55:52 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create base URL API
 export const API = axios.create({
   baseURL: process.env.REACT_APP_BASEURL, // Get REACT_APP_BASEURL from env here ...
-// });
+});
 // export const API = axios.create({
 //   baseURL: "http://localhost:5000/api/v1/"
 // });
